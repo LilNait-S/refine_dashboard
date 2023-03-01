86cc2e29e52b025114b3ecdadb7b5c2c91857dd1
LilNait-S <style.damn.falling@gmail.com>
2023-02-28 22:10:24 -0500

    first commit

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,47 @@
+import User from "../mongodb/models/user.js";
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).limit(req.query._end);
+
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { name, email, avatar } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(200).json(userExists);
+
+    const newUser = await User.create({
+      name,
+      email,
+      avatar,
+    });
+
+    res.status(200).json(newUser);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+const getUserInfoByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id }).populate("allProperties");
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+export { getAllUsers, createUser, getUserInfoByID };
